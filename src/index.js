706bf87e3ea044c114dbead40e36fa986fa7f047
Adarsh <axdrsh@gmail.com>
2025-07-05 02:04:23 +0530

    connected to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// require("dotenv").config({path: "./env"});
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
